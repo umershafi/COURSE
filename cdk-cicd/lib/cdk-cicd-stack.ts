b7ae1b5ae2f75ca0ee4df2f8e813431f0a870ebf
Mohammad Shafi <umershafi02@gmail.com>
2024-10-14 09:38:44 -0700

    code pipeline

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,10 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('umershafi/COURSE', 'cicd'),
+        commands: [
+          'cd cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'ckd-cicd/cdk.out'
+      })
+    })
   }
 }
